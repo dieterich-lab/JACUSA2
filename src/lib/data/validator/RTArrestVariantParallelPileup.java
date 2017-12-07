4e8529d23ed3a05577dd4764814c772ad924fbf3
michael <michael.piechotta@gmail.com>
2017-12-07 03:03:29 +0100

    rt-arrest now outputs sites with no arrest but base substitutions

--- src/lib/data/validator/RTArrestVariantParallelPileup.java
+++ src/lib/data/validator/RTArrestVariantParallelPileup.java
@@ -4,16 +4,24 @@ import lib.data.AbstractData;
 import lib.data.ParallelData;
 import lib.data.has.hasBaseCallCount;
 import lib.data.has.hasReadInfoCount;
+import lib.data.has.hasReferenceBase;
 
-public class RTArrestVariantParallelPileup<T extends AbstractData & hasBaseCallCount & hasReadInfoCount>
+public class RTArrestVariantParallelPileup<T extends AbstractData & hasBaseCallCount & hasReferenceBase & hasReadInfoCount>
 implements ParallelDataValidator<T> {
 	
-	public RTArrestVariantParallelPileup() {}
+	private VariantSiteValidator<T> variantSite;
+	
+	public RTArrestVariantParallelPileup() {
+		variantSite = new VariantSiteValidator<T>();
+	}
+	
 
 	@Override
 	public boolean isValid(final ParallelData<T> parallelData) {
-		return parallelData.getCombinedPooledData().getReadInfoCount().getArrest() > 0 &&
-				parallelData.getCombinedPooledData().getReadInfoCount().getThrough() > 0;
+		T combinedPooledData = parallelData.getCombinedPooledData();
+		
+		return variantSite.isValid(parallelData) || combinedPooledData.getReadInfoCount().getArrest() > 0 &&
+				combinedPooledData.getReadInfoCount().getThrough() > 0;
 	}
 
 }
