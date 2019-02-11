ff6d0db43a78e8bddb85cf5eddcb1e62f6274fdc
Michael Piechotta <michael.piechotta@gmail.com>
2019-02-11 04:00:15 +0100

    Automatic version bump by git_helper.sh

--- src/jacusa/VersionInfo.java
+++ src/jacusa/VersionInfo.java
@@ -3,7 +3,7 @@ package jacusa;
 public final class VersionInfo {
 
 	public final static String BRANCH 	= "develop";
-	public final static String TAG 		= "2.0.0-BETA31";
+	public final static String TAG 		= "2.0.0-BETA32";
 
 	private VersionInfo() {
 		throw new AssertionError();
