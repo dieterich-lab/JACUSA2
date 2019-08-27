82c535915d89c980ca4d49431b4b7eedce62ad3a
qwang <qwang-big>
2019-08-27 18:30:45 +0200

    add -1bp counter of insertions

--- src/lib/data/global/Insertions.java
+++ src/lib/data/global/Insertions.java
@@ -3,13 +3,15 @@ package lib.data.global;
 public class Insertions {
 	// A, C, G, T
 	private static int[] ins = new int[4]; // insertion sequences
-	private static int[] ups = new int[4]; // upstream sequences
+	private static int[][] ups = new int[4][4]; // upstream sequences
 
 	public static void addIns(String s) {
-		BaseCounts.add(ins, s);
+		for(char ch : s.toCharArray())
+			BaseCounts.add(ins, ch);
 	}
 
 	public static void addUps(String s) {
-		BaseCounts.add(ups, s);
+		BaseCounts.add(
+				ups[BaseCounts.i(s.charAt(0))], s.charAt(1));
 	}
 }
